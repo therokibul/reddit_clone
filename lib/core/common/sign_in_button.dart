72b3c7e3988da7bcbcfa15bc0b219554ba119a6a
rokibul <therokibul@gmail.com>
2024-02-23 13:20:30 +0600

    Google Sign In Firebase Authentication

--- lib/core/common/sign_in_button.dart
+++ lib/core/common/sign_in_button.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit/core/constants/constants.dart';
+import 'package:reddit/features/auth/controller/auth_controller.dart';
 
-class SingInButton extends StatelessWidget {
+class SingInButton extends ConsumerWidget {
   const SingInButton({super.key});
 
+  void singInWithGoogle(WidgetRef ref) {
+    ref.read(authControllerProvider).signInWithGoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return ElevatedButton.icon(
-      onPressed: () {},
+      onPressed: () => singInWithGoogle(ref),
       icon: Image.asset(
         Constants.googlePath,
         width: 35,
